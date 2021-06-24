8135ddb3c5640e216bae506263be71a5ced0a075
Luisparra201140004 <86138551+Luisparra201140004@users.noreply.github.com>
2021-06-24 17:36:28 -0600

    Ejercicio 5 aplicaciones de arreglos
    En este apartado esta el ejercicio numero 5

---
+++ Ejercicio 5/Ejercicio 5/Class1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_5
+{
+    class Taller_Mecanico 
+    {
+         
+        private string[,] carros;
+        private List<string> Carros_Acumulados;
+        private List<int> Monto;
+        private int Indice;
+        private int indiceS;
+           
+        public Taller_Mecanico(int n)
+        {
+            
+            N = n;
+            Carros = new string[N, 2];
+            MONTO = new List<int>();
+            CarrosAcumulados = new List<string>();
+            IndiceS = 0;
+            INDICE = 0;
+            for (int rows = 0; rows < N; rows++)
+            {
+                for (int columns = 0; columns < 2; columns++)
+                {
+                    Carros[rows, columns] = " - ";
+                }
+            }
+        }
+        
+        public Boolean Meter_Auto(string Placas, string Nombre_del_Dueño)
+        {
+            if (INDICE < N)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (Carros[i, 0] == " _ ")
+                    {
+                        INDICE = i; break;
+                    }
+                }
+                Carros[INDICE, 0] = Placas;
+                Carros[INDICE, 1] = Nombre_del_Dueño; string value = $"{Placas}, {Nombre_del_Dueño}";
+                CarrosAcumulados.Add(value); return true;
+            }
+            return false;
+        }
+        public Boolean Sacar_Carro()
+        {
+            if (INDICE - 1 >= 0)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (Carros[i, 0] != " _ ")
+                    {
+                        Carros[i, 0] = Carros[i + 1, 0];
+                        Carros[i, 1] = Carros[i + 1, 1];
+                    }
+                }
+                Carros[N - 1, 0] = " - "; Carros[N - 1, 1] = " - "; return true;
+            }
+            return false;
+        }
+        public string EspacioDisponible()
+        {
+            String ans = "";
+            for (int rows = 0; rows < N; rows++)
+            {
+                ans += carros[rows, 0] + " , "; ans += carros[rows, 1] + "\n";
+            }
+            return ans;
+        }
+        public void Cobrar(int monto)
+        {
+            MONTO.Add(monto);
+        }
+        public int[] GetMonto()
+        {
+            int[] Monto_A = MONTO.ToArray(); return Monto_A;
+        }
+        public string[] GetCarrosAcumulados()
+        {
+            string[] ACarrosAcumulados = CarrosAcumulados.ToArray(); return ACarrosAcumulados;
+        }
+            
+        public string[,] Carros
+        {
+            get => carros; set => carros = value;
+        }
+        public int N
+        {
+            get;
+        }
+        public List<int> MONTO
+        {
+            get => Monto; set => Monto = value;
+        }
+        public int INDICE
+        {
+            get => Indice; set => Indice = value;
+        }
+        public List<string> CarrosAcumulados
+        {
+            get => Carros_Acumulados; set => Carros_Acumulados = value;
+        }
+        public int IndiceS { get => indiceS; set => indiceS = value; }
+
+    }
+}
---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            Console.WriteLine("Taller mecanico con capacidad de 7 autos");         
+            Taller_Mecanico taller = new Taller_Mecanico(7);
+            Console.WriteLine(taller.EspacioDisponible());    
+            Console.WriteLine("Espacios disponibles en total 7");
+            Console.WriteLine("Para salir del programa inserte la letra C");
+            char ans; char salir = 'f';
+            while (salir != 'C')
+            {
+                Console.WriteLine("¿Seleccione lo que desea realizar?");
+                Console.WriteLine("A) Meter un auto al taller \nB) sacar un auto del taller \n"); Console.WriteLine("Puede seleccionar una opcion por favor: ");
+                ans = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+               
+                Console.Clear(); switch (ans)
+                {
+                    case 'A':
+                        {
+                            Console.WriteLine("Usted a seleccionado para meter un nuevo auto");
+                            Console.Write("Por favor introduce las placas del auto: "); Console.ForegroundColor = ConsoleColor.White; string placa = Convert.ToString(Console.ReadLine());
+                            Console.Write("Favor de introducir el nombre del dueño del auto: "); Console.ForegroundColor = ConsoleColor.White; string nombre = Convert.ToString(Console.ReadLine());
+                            if (taller.Meter_Auto(placa, nombre) == false)
+                            {
+                                Console.WriteLine("El taller esta lleno");
+                                Console.ReadKey();
+                            }
+                            Console.Clear();
+                            Console.WriteLine("La tabla ha actualizado nuevos registros");
+                            Console.WriteLine(taller.EspacioDisponible());
+                            Console.ReadKey(); break;
+                        }
+                    case 'B':
+                        {
+                            Console.WriteLine("Usted a entrado a la seccion de sacar un veiculo"); if (taller.Sacar_Carro() == false)
+                            {
+                                Console.WriteLine("El taller esta vacio");
+                                Console.ReadKey();
+                            }
+                            Console.WriteLine("Datos del taller actualizados"); Console.WriteLine(taller.EspacioDisponible());
+                            Console.ReadKey(); break;
+                        }
+                }
+                Console.Clear();
+                Console.WriteLine("Si decea salir del programa seleccione la letra C, si decea continuar introdusca la letra n");
+                salir = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                Console.Clear();
+            }
+        }
+    }
+}
